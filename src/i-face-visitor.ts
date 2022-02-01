3b939a53862890a62211b98353e7de05c787dbcb
Maxim Volkov <mx_ruz_net@hotmail.com>
2022-02-01 13:35:47 +0300

    adds method to initalized state

--- src/i-face-visitor.ts
+++ src/i-face-visitor.ts
@@ -2,4 +2,5 @@ import { ServiceLogRecord } from './i-face-service-log';
 
 export interface ServiceLogVisitor<T>{
   visitLogRecord(state: T, record: ServiceLogRecord): T;
+  getInitialState():T;
 }
